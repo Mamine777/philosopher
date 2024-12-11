8c8831af038e23704cf65c4ddfb7c492364a7d70
Mohamed Amine Kariouh <Mamine777>
2024-12-11 19:59:05 +0100

    add

--- philo/init.c
+++ philo/init.c
@@ -3,37 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mokariou <mokariou@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mokariou <mokariou@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 13:05:19 by mokariou          #+#    #+#             */
-/*   Updated: 2024/12/11 17:56:42 by mokariou         ###   ########.fr       */
+/*   Updated: 2024/12/11 19:58:43 by mokariou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	*monitor_routine(void *arg)
-{
-	t_table	*table;
-	int		i;
-
-	table = (t_table *)arg;
-	while (!table->stop_simulation)
-	{
-		i = -1;
-		while (++i < table->num_philosophers)
-		{
-			if (get_time() - table->philo[i].last_meal >= table->schedule.time_to_die)
-            {
-                table->stop_simulation = 1;
-                printf("Philosopher %d has died\n", table->philo[i].id);
-                return (NULL);
-            }
-		}
-		usleep(1000);
-	}
-	return (NULL);
-}
 void	*routine(void *arg)
 {
 	t_philosopher	*philo;
@@ -47,13 +25,58 @@ void	*routine(void *arg)
 		printf("philosopher %d has taken a fork\n", philo->id);
 		printf("philosopher %d is eating\n", philo->id);
 		u_got_knocked_out(philo->eat_time);
+		pthread_mutex_lock(&philo->table->lock);
 		philo->last_meal = get_time();
 		philo->count_meals++;
+		pthread_mutex_unlock(&philo->table->lock);
 		pthread_mutex_unlock(philo->right_fork);
 		pthread_mutex_unlock(philo->left_fork);
 		printf("philosopher %d is sleeping\n", philo->id);
 		u_got_knocked_out(philo->sleep_time);
 		printf("philosopher %d is thinking\n", philo->id);
+		pthread_mutex_lock(&philo->table->lock);
+		if (philo->table->stop_simulation)
+			break ;
+		pthread_mutex_unlock(&philo->table->lock);
+	}
+	return (NULL);
+}
+
+void	*monitor_routine(void *arg)
+{
+	t_table	*table;
+	int		i;
+	int		all_ate;
+
+	table = (t_table *)arg;
+	while (1)
+	{
+		all_ate = 1;
+		i = -1;
+		while (++i < table->num_philosophers)
+		{
+			pthread_mutex_lock(&table->lock);
+			if ((get_time()
+					- table->philo[i].last_meal) >= table->schedule.time_to_die)
+			{
+				table->stop_simulation = 1;
+				printf("Philosopher %d has died\n", table->philo[i].id);
+				pthread_mutex_unlock(&table->lock);
+				return (NULL);
+			}
+			if (table->schedule.num_of_to_eat > 0
+				&& table->philo[i].count_meals < table->schedule.num_of_to_eat)
+				all_ate = 0;
+			pthread_mutex_unlock(&table->lock);
+		}
+		if (all_ate)
+		{
+			pthread_mutex_lock(&table->lock);
+			table->stop_simulation = 1;
+			pthread_mutex_unlock(&table->lock);
+			return (NULL);
+		}
+		usleep(5000);
 	}
 	return (NULL);
 }
@@ -64,14 +87,17 @@ static void	create_threads(t_table *table)
 	pthread_t	monitor;
 
 	i = -1;
-    if (pthread_create(&monitor, NULL, monitor_routine, table))
-    {
-        printf("Error: Failed to create monitor thread\n");
-        return;
-    }
+	if (pthread_create(&monitor, NULL, monitor_routine, table))
+		return ;
 	while (++i < table->num_philosophers)
 	{
-		pthread_create(&table->threads[i], NULL, &routine, &table->philo[i]);
+		if (pthread_create(&table->threads[i], NULL, routine,
+				&table->philo[i]) != 0)
+		{
+			printf("Failed to create philosopher thread");
+			table->stop_simulation = 1;
+			break ;
+		}
 	}
 	i = -1;
 	while (++i < table->num_philosophers)
@@ -84,6 +110,7 @@ static void	create_threads(t_table *table)
 	{
 		pthread_mutex_destroy(&table->forks[i]);
 	}
+	pthread_mutex_destroy(&table->lock);
 }
 
 void	init_philo(t_table *table)
@@ -91,6 +118,8 @@ void	init_philo(t_table *table)
 	int	i;
 
 	i = -1;
+	if (pthread_mutex_init(&table->lock, NULL))
+		return ;
 	while (++i < table->num_philosophers)
 	{
 		if (pthread_mutex_init(&table->forks[i], NULL))
@@ -105,10 +134,11 @@ void	init_philo(t_table *table)
 		table->philo[i].eat_time = table->schedule.eat_time;
 		table->philo[i].sleep_time = table->schedule.sleep_time;
 		table->philo[i].num_of_to_eat = table->schedule.num_of_to_eat;
+		table->philo[i].last_meal = get_time();
 		table->philo[i].left_fork = &table->forks[i];
 		table->philo[i].right_fork = &table->forks[(i + 1)
 			% table->num_philosophers];
-		table->philo[i].start_simulation = get_time();
+		table->philo[i].table = table;
 	}
 	create_threads(table);
 }
@@ -121,13 +151,13 @@ void	fill_table(t_table *table, char **av)
 	table->schedule.sleep_time = ft_atoi(av[4]) * 1e3;
 	table->stop_simulation = 0;
 	if (av[5])
-		table->schedule.num_of_to_eat = ft_atoi(av[5]) * 1e3;
+		table->schedule.num_of_to_eat = ft_atoi(av[5]);
 	else
 		table->schedule.num_of_to_eat = -1;
 	if (table->num_philosophers <= 0 || table->schedule.time_to_die <= 0
 		|| table->schedule.eat_time <= 0 || table->schedule.sleep_time <= 0)
 	{
 		printf("Error: Invalid input values\n");
-		return ;
+		exit(1);
 	}
 }
--- philo/philo.h
+++ philo/philo.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mokariou <mokariou@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mokariou <mokariou@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 12:45:22 by mokariou          #+#    #+#             */
-/*   Updated: 2024/12/11 17:55:55 by mokariou         ###   ########.fr       */
+/*   Updated: 2024/12/11 19:45:30 by mokariou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@
 # include <pthread.h>
 # include <stdio.h>
 # include <stdlib.h>
-# include <unistd.h>
 # include <sys/time.h>
+# include <unistd.h>
 
 typedef struct s_schedule
 {
@@ -34,9 +34,8 @@ typedef struct s_philosopher
 	pthread_mutex_t	*right_fork;
 	long			last_meal;
 	int				did_he_eat;// bool
-	long			start_simulation;
 	int				count_meals;
-	t_table			*table;
+	struct s_table	*table;
 //////
 	long			eat_time;
 	long			sleep_time;
@@ -44,14 +43,15 @@ typedef struct s_philosopher
 	long			num_of_to_eat;
 }					t_philosopher;
 
-typedef struct t_table
+typedef struct s_table
 {
 	pthread_mutex_t	forks[200];
 	pthread_t		threads[200];
 	long			num_philosophers;
 	t_philosopher	philo[200];
 	t_schedule		schedule;
-	int 			stop_simulation;
+	int				stop_simulation;
+	pthread_mutex_t lock;
 }					t_table;
 
 /// error_checking
@@ -59,7 +59,7 @@ void				err_exit(const char *str);
 ///*****utils
 int					ft_atoi(const char *str);
 uint16_t			get_time(void);
-void	u_got_knocked_out(long ms);
+void				u_got_knocked_out(long ms);
 
 /// main
 void				init_philo(t_table *table);
